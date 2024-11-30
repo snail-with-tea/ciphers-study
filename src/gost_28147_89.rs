787101e386dd5b9e8ca828e92d7225ec97bc34a9
DimonNeAdekvat <tipounlogick@gmail.com>
2024-12-01 01:50:29 +0300

    small refactor & testing

--- src/gost_28147_89.rs
+++ src/gost_28147_89.rs
@@ -77,8 +77,8 @@ fn sum_m0(a: u32, b: u32) -> u32 {
 }
 
 fn sum_m1(a: u32, b: u32) -> u32 {
-    let mut r = (Wrapping(a) + Wrapping(b)).0 + 1;
-    if u32::MAX - b >= a {
+    let mut r = (Wrapping(a) + Wrapping(b) + Wrapping(1)).0;
+    if u32::MAX - b > a {
         r -= 1;
     }
     r
@@ -92,12 +92,7 @@ fn xor_64(a: u64, b: u64) -> u64 {
     a ^ b
 }
 
-fn round_encode(block: &mut [u8; 8], state: &mut State) {
-    let first = [block[0], block[1], block[2], block[3]];
-    let secnd = [block[4], block[5], block[6], block[7]];
-    state.n_acc[0] = u32::from_be_bytes(first);
-    state.n_acc[1] = u32::from_be_bytes(secnd);
-
+fn round_encode(state: &mut State) {
     for i in 0..24 {
         state.c_sum[0] = sum_m0(state.x_key[i % 8], state.n_acc[0]);
         let r = state.substitute(state.c_sum[0]).rotate_left(11);
@@ -113,15 +108,10 @@ fn round_encode(block: &mut [u8; 8], state: &mut State) {
         state.n_acc[0] = state.c_sum[1];
     }
 
-    *block = (((state.n_acc[1] as u64) << 32) | state.n_acc[0] as u64).to_be_bytes()
+    state.n_acc.swap(0, 1);
 }
 
-fn round_decode(block: &mut [u8; 8], state: &mut State) {
-    let first = [block[0], block[1], block[2], block[3]];
-    let secnd = [block[4], block[5], block[6], block[7]];
-    state.n_acc[0] = u32::from_be_bytes(first);
-    state.n_acc[1] = u32::from_be_bytes(secnd);
-
+fn round_decode(state: &mut State) {
     for i in 0..8 {
         state.c_sum[0] = sum_m0(state.x_key[i % 8], state.n_acc[0]);
         let r = state.substitute(state.c_sum[0]).rotate_left(11);
@@ -137,7 +127,7 @@ fn round_decode(block: &mut [u8; 8], state: &mut State) {
         state.n_acc[0] = state.c_sum[1];
     }
 
-    *block = (((state.n_acc[1] as u64) << 32) | state.n_acc[0] as u64).to_be_bytes()
+    state.n_acc.swap(0, 1);
 }
 
 pub fn simple_repl_encode(data: &mut [u8], key: Key) {
@@ -145,7 +135,10 @@ pub fn simple_repl_encode(data: &mut [u8], key: Key) {
 
     for block in data.chunks_exact_mut(8) {
         // can do unwrap since we are in chunks of 8
-        round_encode(block.try_into().unwrap(), &mut state)
+        let block: &mut [u8; 8] = block.try_into().unwrap();
+        [state.n_acc[0], state.n_acc[1]] = u32x2_from_be(*block);
+        round_encode(&mut state);
+        *block = u32x2_to_be([state.n_acc[0], state.n_acc[1]]);
     }
 }
 
@@ -154,8 +147,21 @@ pub fn simple_repl_decode(data: &mut [u8], key: Key) {
 
     for block in data.chunks_exact_mut(8) {
         // can do unwrap since we are in chunks of 8
-        round_decode(block.try_into().unwrap(), &mut state)
+        let block: &mut [u8; 8] = block.try_into().unwrap();
+        [state.n_acc[0], state.n_acc[1]] = u32x2_from_be(*block);
+        round_decode(&mut state);
+        *block = u32x2_to_be([state.n_acc[0], state.n_acc[1]]);
     }
+}
+
+fn u32x2_from_be(x: [u8; 8]) -> [u32; 2] {
+    let [a, b, c, d, e, f, g, h] = x;
+    [[a, b, c, d], [e, f, g, h]].map(u32::from_be_bytes)
+}
+
+fn u32x2_to_be(x: [u32; 2]) -> [u8; 8] {
+    let [[a, b, c, d], [e, f, g, h]] = x.map(u32::to_be_bytes);
+    [a, b, c, d, e, f, g, h]
 }
 
 #[test]
@@ -176,6 +182,31 @@ fn eqeqeqeq() {
 fn summ() {
     let r = sum_m1(0x01, 0x0E);
     assert_eq!(r, 0x0F);
-    let r = sum_m1(0xFFFFFFFF, 0x00000003);
+    let r = sum_m1(0xFFFFFFFF, 0x03);
     assert_eq!(r, 0x03);
+    let r = sum_m1(0xFFFFFFFF, 0x00);
+    assert_eq!(r, 0x00);
+}
+
+fn sum_t1(a: u32, b: u32) -> u32 {
+    let r = a as u64 + b as u64;
+    (r % u32::MAX as u64) as u32
+}
+
+//this one emits less ams instructions
+fn sum_t2(a: u32, b: u32) -> u32 {
+    let mut r = (Wrapping(a) + Wrapping(b) + Wrapping(1)).0;
+    if u32::MAX - b > a {
+        r -= 1;
+    }
+    r
+}
+
+#[test]
+fn cmp_summ() {
+    for a in u32::MAX - 20..=u32::MAX {
+        for b in 0..16 {
+            assert_eq!(sum_t1(a, b), sum_t2(a, b));
+        }
+    }
 }
